0f1941f705cd3f1dfdfddc1b8b9819d450b19939
Alex Foster <123afoster@gmail.com>
2021-01-15 16:44:35 -0500

    move private aiinfo fxns to ai.cpp

--- game/ai.cpp
+++ game/ai.cpp
@@ -21,6 +21,93 @@ namespace ai
     ICOMMAND(botlimit, "i", (int *n), addmsg(NetMsg_BotLimit, "ri", *n));
     ICOMMAND(botbalance, "i", (int *n), addmsg(NetMsg_BotBalance, "ri", *n));
 
+
+    void aiinfo::clearsetup()
+    {
+        weappref = Gun_Rail;
+        spot = target = vec(0, 0, 0);
+        lastaction = lasthunt = lastcheck = enemyseen = enemymillis = blocktime = huntseq = blockseq = targtime = targseq = lastaimrnd = 0;
+        lastrun = jumpseed = lastmillis;
+        jumprand = lastmillis+5000;
+        targnode = targlast = enemy = -1;
+    }
+
+    void aiinfo::clear(bool prev)
+    {
+        if(prev)
+        {
+            memset(prevnodes, -1, sizeof(prevnodes));
+        }
+        route.setsize(0);
+    }
+
+    void aiinfo::wipe(bool prev)
+    {
+        clear(prev);
+        state.setsize(0);
+        addstate(AIState_Wait);
+        trywipe = false;
+    }
+
+    void aiinfo::clean(bool tryit)
+    {
+        if(!tryit)
+        {
+            becareful = dontmove = false;
+        }
+        targyaw = randomint(360);
+        targpitch = 0.f;
+        tryreset = tryit;
+    }
+
+    void aiinfo::reset(bool tryit)
+    {
+        wipe();
+        clean(tryit);
+    }
+
+    aistate &aiinfo::addstate(int t, int r, int v)
+    {
+        return state.add(aistate(lastmillis, t, r, v));
+    }
+
+    void aiinfo::removestate(int index)
+    {
+        if(index < 0)
+        {
+            state.pop();
+        }
+        else if(state.inrange(index))
+        {
+            state.remove(index);
+        }
+        if(!state.length())
+        {
+            addstate(AIState_Wait);
+        }
+    }
+
+    aistate &aiinfo::getstate(int idx)
+    {
+        if(state.inrange(idx))
+        {
+            return state[idx];
+        }
+        return state.last();
+    }
+
+    aistate &aiinfo::switchstate(aistate &b, int t, int r, int v)
+    {
+        if((b.type == t && b.targtype == r) || (b.type == AIState_Interest && b.targtype == AITravel_Node))
+        {
+            b.millis = lastmillis;
+            b.target = v;
+            b.reset();
+            return b;
+        }
+        return addstate(t, r, v);
+    }
+
     float aiinfo::viewdist(int skill)
     {
         if(skill <= 100)
@@ -1836,11 +1923,8 @@ namespace ai
     VAR(showwaypoints, 0, 0, 1); //display waypoint locations in edit mode
     VAR(showwaypointsradius, 0, 200, 10000); //maximum distance to display (200 = 25m)
 
-    const char *stnames[AIState_Max] = {
-        "wait", "defend", "pursue", "interest"
-    }, *sttypes[AITravel_Max+1] = {
-        "none", "node", "player", "affinity", "entity"
-    };
+    const char *stnames[AIState_Max]    = { "wait", "defend", "pursue", "interest" },
+               *sttypes[AITravel_Max+1] = { "none", "node", "player", "affinity", "entity" };
     void render()
     {
         if(aidebug > 1)
