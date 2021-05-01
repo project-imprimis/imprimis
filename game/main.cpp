5dd40ba9015d3eb82d1d823747a444d6b1f42b5a
Alex Foster <123afoster@gmail.com>
2021-04-30 21:20:37 -0700

    roll version string over to alpha 5

--- game/main.cpp
+++ game/main.cpp
@@ -81,7 +81,7 @@ void updateenginevalues()
     dynents = dyns;
 }
 
-constexpr const char * versionstring = "Alpha 4 \"Dragontail\"";
+constexpr const char * versionstring = "Alpha 5 \"Eldorado\"";
 
 //sets engine constants that need information from the game
 //as a result, all values set here are global variables defined elsewhere in
