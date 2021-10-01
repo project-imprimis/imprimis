990f451a9823ebbe5735743cf2a1c6f910b63c71
Alex Foster <123afoster@gmail.com>
2021-09-30 23:31:04 -0700

    roll version string over to alpha 10

--- game/main.cpp
+++ game/main.cpp
@@ -81,7 +81,7 @@ void updateenginevalues()
     dynents = dyns;
 }
 
-constexpr const char * versionstring = "Alpha 9 \"Icebox\"";
+constexpr const char * versionstring = "Alpha 10 \"Jumbo\"";
 
 //sets engine constants that need information from the game
 //as a result, all values set here are global variables defined elsewhere in
