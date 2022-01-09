4b60288abd59c957c1a851e5e565f8f6fe81b27e
Luis David Licea Torres <luislicea1011@hotmail.com>
2022-01-08 23:10:34 -0600

    Speed up Imprimis startup.
    Reduced the startup time by at least one second by:
      - Moving error-handling code into the `dependency_installer.py` file.
      - Defining the main function signature instead of the entire function.
      - Importing module classes instead of the whole modules.
    
    Also, the `dependency_installer.py` file also tries to install cppyy if
    it not found, and tries to compile imprimis.so if it does not exist.

--- imprimis.py
+++ imprimis.py
@@ -1,126 +1,114 @@
-# To detect operating sytem type.
-from sys import platform
-# To install cppyy module if it does not exist.
-import os
 # To join paths in a cross-platform way.
 from os import path
-# To access arguments passed to the script.
-import sys
+# To install missing Python modules.
+import dependency_installer
+# To detect operating sytem type, and to access arguments passed to the script.
+from sys import platform, argv
 
 try:
-  # Try to import cppyy to call C++ from Python.
-  import cppyy
+    # Try to import cppyy to call C++ from Python.
+    from cppyy import add_include_path, load_library, cppdef, gbl
 
 except ImportError:
-  # If module could not be imported, prompt user to install it.
-  intall = ('The cppyy module is required. Install? [y/n] ')
-
-  # Convert to lowercase.
-  install = install.lower()
-
-  # If install is 'y' or 'yes':
-  if install in set(['', 'y','ye','yes']):
-
-    # Install cppyy.
-    print('Installing cppyy.')
-    if platform == 'linux':
-        # If Linux, use ~/.imprimis
-        installation_message = os.system('sudo python3 -m pip install cppyy')
-
-    elif platform == 'win32':
-        # If Windows, use $HOME\My Games\Imprimis
-        installation_message = os.system('python -m pip install cppyy')
-
-    try:
-      # Try to import cppyy after it has been isntalled.
-      import cppyy
-
-    except ImportError:
-      # Throw error message if installation failed.
-      raise ModuleNotFoundError('Could not install cppyy.')
+    # If the module could not be imported, prompt user to install it.
+    dependency_installer.install_required_modules()
 
 # Get home directory.
 def get_home_directory() -> str:
-  ''' Return the game's home directory. '''
+    '''Return the game's home directory.'''
 
-  if platform == 'linux':
-      # If Linux, use ~/.imprimis
-      return str(path.join(path.expanduser('~'), '.imprimis'))
+    if platform == 'linux':
+        # If Linux, use ~/.imprimis
+        return str(path.join(path.expanduser('~'), '.imprimis'))
 
-  elif platform == 'win32':
-      # If Windows, use $HOME\My Games\Imprimis
-      return str(path.join(path.expanduser('~'), 'My Games', 'Imprimis'))
+    elif platform == 'win32':
+        # If Windows, use $HOME\My Games\Imprimis
+        return str(path.join(path.expanduser('~'), 'My Games', 'Imprimis'))
 
-  # Return an error message for any other operating systems.
-  raise RuntimeError('Only Windows and Linux operating systems are supported.')
+    # Return an error message for any other operating systems.
+    raise RuntimeError('Your operating system is not supported.')
 
 def get_imprimis_lib_path() -> str:
-  ''' Return path to imprimis library. '''
+    '''Return path to imprimis library.'''
 
-  # Define library name.
-  imprimis = 'imprimis'
+    # Define library name.
+    imprimis = 'imprimis'
 
-  # Define path.
-  imprimis_lib_path = ""
+    # Define path.
+    imprimis_lib_path = ""
 
-  if platform == 'linux':
-      # If Linux, use ./imprimis.so
-      imprimis_lib_path = str(path.join(path.curdir, f'{imprimis}.so'))
+    if platform == 'linux':
+        # If Linux, use ./imprimis.so
+        imprimis_lib_path = str(path.join(path.curdir, f'{imprimis}.so'))
 
-  elif platform == 'win32':
-      # If Windows, use .\bin64\imprimis.lib
-      imprimis_lib_path = str(path.join(path.curdir, 'bin64', f'{imprimis}.lib'))
+    elif platform == 'win32':
+        # If Windows, use .\bin64\imprimis.lib
+        imprimis_lib_path = str(path.join(path.curdir, 'bin64', f'{imprimis}.lib'))
 
-  else:
-    # Return an error message for any other operating systems.
-    raise RuntimeError('Only Windows and Linux operating systems are supported.')
+    else:
+        # Return an error message for any other operating systems.
+        raise RuntimeError('Your operating system is not supported.')
 
-  if not path.isfile(imprimis_lib_path):
-    print('''Your platform does not have a pre-compiled imprimis.so library.
-    Please follow the following steps to build a native client:
-    1) Type 'make' to co compile the imprimis.so library.
-    2) Ensure you have the engine binary at /usr/lib/libprimis.so.
-    3) Ensure you have the SDL2, SDL2-image, SDL2-mixer, and OpenGL libraries installed.
-    4) If the build succeeds, run this script again.''')
+    # Prompt the user to compile Imprimis library if it does not exist.
+    if not path.isfile(imprimis_lib_path):
+        dependency_installer.compile_imprimis_library()
 
-    # Return an error.
-    raise RuntimeError(f'Unable to load {imprimis_lib_path}.')
-
-  # Return the path.
-  return imprimis_lib_path
-
+    # Return the path.
+    return imprimis_lib_path
 
 def get_file_path(name: str = "main.cpp") -> str:
-  ''' Return path to main.cpp, or any file in the game folder. '''
+    '''Return path to main.cpp, or any file in the game folder.'''
 
-  # Get file path.
-  file_path = str(path.join(path.curdir, 'game', name))
+    # Get file path.
+    file_path = str(path.join(path.curdir, 'game', name))
 
-  # Check that the file exists.
-  if path.isfile(file_path):
-    return file_path
+    # Check that the file exists.
+    if path.isfile(file_path):
+        return file_path
 
-  # Else the file does not exist.
-  raise RuntimeError(f'The file {file_path} does not eixst.')
+    # Else the file does not exist.
+    raise RuntimeError(f'The file {file_path} does not eixst.')
 
-# Load shared library.
-cppyy.load_library(get_imprimis_lib_path())
+def main():
+    '''Run Imprimis in Windows or Linux.'''
+    # Specify the location of the SDL.h shared library.
+    if platform == "linux":
+        # This solution takes less than 0.1 seconds, but isn't portable.
+        # The header is already located in the shared library, but cppyy needs
+        # to know its location. Therefore it is faster to include the path, than
+        # to include the header.
+        add_include_path('/usr/include/SDL2')
+        # This solution takes about 0.4 seconds, but is portable.
+        # Including the header is much slower than including the path.
+        # include('SDL2/SDL.h')
 
-# Load the main function.
-cppyy.include(get_file_path('main.cpp'))
+    elif platform == "win32":
+        # TODO: Test if this is one of these headers is needed in windows. It
+        # may be possible to specify the header path, as it faster for cppyy.
+        # add_include_path('C:\\..some path..\\SDL2\\SDL.h')
+        # include('SDL.h')
+        # include('SDL2\SDL.h')
+        pass
 
-# Print the settings folder.
-print('Game folder:', get_home_directory())
+    # Load the Imprimis shared library.
+    load_library(get_imprimis_lib_path())
 
-# Sart the executable with the given parameters: The home directory (-u) and log
-# file name (-g). The last argument sys.argv[1:] represents all the arguments
-# passed when calling the script. For example in `script.py arg1 arg2 arg3`, the
-# symbol sys.argv[1:] would represent arg1, arg2, and arg3. The 0th value is the
-# name of the script, so sys.arg[0] would be `script.py`.
-# If no log file name is specified, the logs will be printed directly on the
-# terminal. If a log file name is given, the log file will be saved in the
-# current directory.
-parameters = ['-u' + get_home_directory(), '-glog.txt'] + sys.argv[0:]
+    # Sart the executable with the given parameters: The home directory (-u) and log
+    # file name (-g). The last argument sys.argv[1:] represents all the arguments
+    # passed when calling the script. For example in `script.py arg1 arg2 arg3`, the
+    # symbol sys.argv[1:] would represent arg1, arg2, and arg3. The 0th value is the
+    # name of the script, so sys.arg[0] would be `script.py`.
+    # If no log file name is specified, the logs will be printed directly on the
+    # terminal. If a log file name is given, the log file will be saved in the
+    # current directory.
+    parameters = ['-u' + get_home_directory(), '-glog.txt'] + argv[0:]
 
-# Call the main function and pass the arguments.
-cppyy.gbl.main(len(parameters), parameters)
+    # Since main.cpp does not have a header file, define the signature of the
+    # main function.
+    cppdef("int main(int, char**);")
+
+    # Call the main function and pass the arguments.
+    gbl.main(len(parameters), parameters)
+
+if __name__ == '__main__':
+    main()
