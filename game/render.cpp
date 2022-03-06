e565fef89f4bcad978b690f5d9e9b0acc54c5d41
Alex Foster <123afoster@gmail.com>
2022-03-06 01:35:32 -0800

    use reference to cube in hmapsel

--- game/render.cpp
+++ game/render.cpp
@@ -936,7 +936,7 @@ namespace game
 
                 if(hmapedit==1 && DIM_COORD(horient) == (camdir[DIMENSION(horient)]<0))
                 {
-                    hmapsel = hmap::isheightmap(horient, DIMENSION(horient), false, c);
+                    hmapsel = hmap::isheightmap(horient, DIMENSION(horient), false, *c);
                     if(hmapsel)
                     {
                         od = DIMENSION(orient = horient);
