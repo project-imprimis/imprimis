d5bdc857b126127d66078dda9094cab782fc7b41
Alex Foster <123afoster@gmail.com>
2022-05-01 12:52:03 -0700

    roll version string to alpha 17

--- game/main.cpp
+++ game/main.cpp
@@ -81,7 +81,7 @@ void updateenginevalues()
     dynents = dyns;
 }
 
-constexpr const char * versionstring = "Alpha 16 \"Porcupine\"";
+constexpr const char * versionstring = "Alpha 17 \"Queets\"";
 
 //sets engine constants that need information from the game
 //as a result, all values set here are global variables defined elsewhere in
