57760c325396443a0b7386826fb447c2a98232ac
Alex Foster <123afoster@gmail.com>
2026-04-12 23:58:11 -0700

    roll version string over to v0.64a

--- game/main.cpp
+++ game/main.cpp
@@ -71,7 +71,7 @@ void updateenginevalues()
     dynents = dyns;
 }
 
-constexpr const char * versionstring = "Alpha 63 \"Cass\"";
+constexpr const char * versionstring = "Alpha 64 \"Pounder\"";
 
 //sets engine constants that need information from the game
 //as a result, all values set here are global variables defined elsewhere in
